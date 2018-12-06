2453bdcf7a94d1f977fb6b559d356e604b6e6583
Nondh Panithanpaisal <nondh@modv-ve701-1241.eduroam.wlan.private.upenn.edu>
2018-12-06 18:54:25 -0500

    update

--- mock_redclump.py
+++ mock_redclump.py
@@ -47,7 +47,7 @@ gcentric = c.transform_to(coord.Galactocentric)
 gcentric.representation = 'cylindrical'
 
 #r_ensemble = np.array([0.1,0.5,1,1.5,2])*u.kpc
-r_ensemble = np.linspace(0.1, 3, 31)
+r_ensemble = np.linspace(0.1, 3, 31)*u.kpc
 z_ensemble = np.array([2])*u.kpc
 dis_array = np.zeros((len(r_ensemble), len(z_ensemble)))
 i = 0
